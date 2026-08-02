21972a1eb0cb793cbfa7e11374177ddbf7a25d0b
Alex potter <alexpandiyan1914@gmail.com>
2026-08-02 16:21:08 +0530

    fix: minor bug

--- src/main/java/com/campusfound/auth/dto/AuthResponse.java
+++ src/main/java/com/campusfound/auth/dto/AuthResponse.java
@@ -8,6 +8,9 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class AuthResponse {
-    private String message;
+
     private String token;
+
+    private String message;
+
 }
